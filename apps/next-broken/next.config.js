fa257e451617b9465d0dcf98969ac5018fd82574
tar-aldev <frontdev.at@gmail.com>
2023-12-13 21:18:36 +0200

    use terser minimizer

--- apps/next-broken/next.config.js
+++ apps/next-broken/next.config.js
@@ -2,11 +2,26 @@
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.optimization.minimizer = [
+      new TerserPlugin({
+        terserOptions: {
+          keep_classnames: true,
+        },
+      }),
+    ];
+
+    return config;
+  },
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
